ebef59876a149c77dcf106c3e92f638d2331cf8c
Muhammad Azis <azisapisl@gmail.com>
2026-04-05 01:50:34 +0700

    Penambahan Source Code

--- P3_MuhammadAzis/latihan1.java
+++ P3_MuhammadAzis/latihan1.java
@@ -1,5 +1,22 @@
 package P3_MuhammadAzis;
 
+import java.util.Scanner;
+
 public class latihan1{
-    
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ketik nama anda: ");
+        String nama = input.nextLine();
+        System.out.print("Ketik Nilai Desimal: ");
+        double nilaiAsli = input.nextDouble();
+        
+        int nilaiBulat = (int) nilaiAsli;
+
+        System.out.println("Hello "+ nama);
+        System.out.println("Nilai Asli: "+ nilaiAsli);
+        System.out.println("Nilai Setengah Casting: "+ nilaiBulat);
+
+        input.close();
+
+    }
 }
