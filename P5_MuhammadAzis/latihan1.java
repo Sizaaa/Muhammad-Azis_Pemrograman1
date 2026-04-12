711847856848fbb6e05ae6c19b4b183816ec7326
Muhammad Azis <azisapisl@gmail.com>
2026-04-12 23:22:59 +0700

    Update code Pertemuan 5

--- P5_MuhammadAzis/latihan1.java
+++ P5_MuhammadAzis/latihan1.java
@@ -1,5 +1,61 @@
 package P5_MuhammadAzis;
 
+import java.util.Scanner;
+
 public class latihan1 {
-    
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        String namaBarang;
+        int harga, jumlah, uang, total;
+
+        System.out.println("=== PROGRAM KASIR SEDERHANA ===");
+        System.out.print("Nama Barang: ");
+        namaBarang = input.nextLine();
+        System.out.print("Harga Barang: ");
+        harga = input.nextInt();
+        System.out.print("Jumlah Beli: ");
+        jumlah = input.nextInt();
+        System.out.print("Uang diterima: ");
+        uang = input.nextInt();
+
+        total = harga * jumlah;
+
+        System.out.println("\n=== PILIH METODE PEMBAYARAN ===");
+        System.out.println("1. Cash");
+        System.out.println("2. E-Wallet");
+        System.out.print("Pilihan: ");
+        int pilihan = input.nextInt();
+        System.out.println("==================================");
+
+        if(pilihan == 1){
+            System.out.println("Pembayaran: Cash");
+            if(total > 100000){
+                total = total - 10000;
+                System.out.println("Dapatkan diskon 10.000!");
+            } else {
+                System.out.println(" Tidak dapat diskon");
+            }
+        } else if (pilihan == 2){
+            System.out.println("Pembayaran: E-Wallet");
+            if(total > 50000){
+                System.out.println("Dapat Cashback kecil!");
+            }
+        } else{
+            System.out.println("Pilihan tidak tersedia");
+        }
+
+        if (uang < total ){
+            System.out.println("Uang tidak cukup!");
+        } else {
+            int kembalian = uang - total;
+            System.out.println("Kembalian: "+ kembalian);
+        }
+        
+        System.out.println("\n=== STRUK ===");
+        System.out.println("Barang: "+ namaBarang);
+        System.out.println("Total Bayar: "+ total);
+        System.out.println("================");
+
+    }
 }
--- P5_MuhammadAzis/latihan3.java
+++ P5_MuhammadAzis/latihan3.java
@@ -1,5 +1,23 @@
 package P5_MuhammadAzis;
 
 public class latihan3 {
-    
+    public static void main(String[] args) {
+        int nilai = 90;
+
+        if(nilai > 50){
+            System.out.println("Nilai di atas 50");
+        }
+        if(nilai >=60){
+            System.out.println("Lulus");
+        } else {
+            System.out.println("Tidak lulus");
+        }
+        if (nilai >=85){
+            System.out.println("Grade A");
+        } else if (nilai >= 70){
+            System.out.println("Grade B");
+        } else {
+            System.out.println("Grade C");
+        }
+    }
 }
